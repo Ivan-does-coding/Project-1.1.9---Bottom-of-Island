a750e7e3e9acbbb71b210b4cffd7bde88c4fd370
IvanParamonov <552004@my.cuhsd.org>
2020-10-23 18:17:43 +0000

    Got rid of the top part (Kai did it)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import turtle as trtl
 
 # The base + fill color of the Island
 
-trtl.pensize(7)
+trtl.pensize(3)
 trtl.penup()
 trtl.setposition(-100,0)
 trtl.pendown()
@@ -25,53 +25,6 @@ trtl.right(22.5)
 trtl.forward(20)
 trtl.color("saddle brown")
 trtl.end_fill()
-
-#The top part outline of the Island 
-
-# The left face of the island
-trtl.color("dim grey")
-trtl.penup()
-trtl.setposition(-117.5,47.5)
-trtl.penup()
-trtl.left(162.5)
-trtl.pendown()
-trtl.forward(40)
-
-#The underpart of the cliff
-for i in range (2):
-  trtl.left(25)
-  trtl.forward(30)
-
-# The left overhang part of the cliff
-for i in range (5):
-  trtl.right(25)
-  trtl.forward(10)
-
-trtl.color("sienna")
-trtl.right(25)
-trtl.forward(10)
-
-
-# The top of the cliff
-trtl.forward(100)
-trtl.left(10)
-trtl.forward(70)
-trtl.left(15)
-trtl.forward(30)
-
-
-# The right cliff of the outline
-for i in range (4):
-  trtl.right(25)
-  trtl.forward(5)
-
-trtl.color("dim grey")
-
-# The right face of the island
-trtl.left(25)
-trtl.forward(30)
-trtl.right(35)
-trtl.forward(35)
 
 wn = trtl.Screen()
 wn.mainloop()
